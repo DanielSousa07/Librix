0a489ed035660a790105f032844fdc353610180b
SINAP <zjoao4529@gmail.com>
2026-05-06 11:21:58 -0300

    videos adicionados nos jogos

--- src/components/quizVideos.js
+++ src/components/quizVideos.js
@@ -1,16 +1,44 @@
 export const perguntas = [
     {
     id: 1,
-    youtubeId: "tktmP632XxY", /* https://www.youtube.com/watch?v=tktmP632XxY */
+    youtubeId: "J3I2gSjO52Q", /* https://www.youtube.com/watch?v=J3I2gSjO52Q */
     pergunta: "Qual letra este sinal representa?",
     opcoes: ["A", "M", "N", "R"],
     correta: "M"
   },
   {
     id: 2,
-    youtubeId: "/videos/B.mp4",
+    youtubeId: "DZqEJ-5HIoc", /* https://www.youtube.com/watch?v=DZqEJ-5HIoc */
     pergunta: "Qual letra este sinal representa?",
     opcoes: ["B", "D", "P", "T"],
     correta: "B"
+  },
+  {
+    id: 3,
+    youtubeId: "s5QtTZhWNE4", /* https://www.youtube.com/watch?v=s5QtTZhWNE4 */
+    pergunta: "Qual letra este sinal representa?",
+    opcoes: ["L", "K", "T", "F"],
+    correta: "F"
+  },
+  {
+    id: 4, 
+    youtubeId: "NPYLv2q1THQ", /* https://www.youtube.com/watch?v=NPYLv2q1THQ */
+    pergunta: "Qual letra este sinal representa?",
+    opcoes: ["J", "I", "P", "E"],
+    correta: "J"
+  },
+  {
+    id: 5,
+    youtubeId: "esHOhai8L8E", /* https://www.youtube.com/watch?v=esHOhai8L8E */
+    pergunta: "Qual letra este sinal representa?",
+    opcoes: ["B", "T", "Y", "F"],
+    correta: "T"
+  },
+  {
+    id: 6,
+    youtubeId: "D5m30OXpoR4", /* https://www.youtube.com/watch?v=D5m30OXpoR4 */
+    pergunta: "Qual letra este sinal representa?",
+    opcoes: ["B", "D", "G", "Q"],
+    correta: "G"
   }
 ]
